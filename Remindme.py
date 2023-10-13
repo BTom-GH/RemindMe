fbba8857053f5748446edeb078541241e305e199
Tom Bloch <tom.bloch@telecomnancy.Eu>
2023-10-13 04:56:17 +0200

    Initial commit

---
+++ Remindme.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
+from plyer import notification
