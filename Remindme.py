75770229398d28182973b0bf9878bc66f14e0643
Tom Bloch <61981635+BTom-GH@users.noreply.github.com>
2024-01-22 11:02:31 +0100

    Changing from kivy to pyqt6

--- Remindme.py
+++
@@ -1,6 +0,0 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
-from plyer import notification
